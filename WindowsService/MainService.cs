dca1760c2145bbd2dd42a6859056c56b92fc98d1
Kirsang <kirsang2013@yandex.ru>
2018-07-25 15:35:45 +0500

    Общая ветвь созданной службы

---
+++ WindowsService/MainService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryClassforServiceWindow;
+
+namespace WindowsService
+{
+    public partial class MainService : ServiceBase
+    {
+
+        LogFile file;
+
+        public MainService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            file = new LogFile();
+
+            file.Write("OnStart");
+        }
+
+        protected override void OnStop()
+        {
+            file.Write("OnStop");
+
+            file.Dispose();
+        }
+
+        protected override void OnPause()
+        {
+            file.Write("OnPause");
+        }
+
+        protected override void OnContinue()
+        {
+            file.Write("OnContinue");
+        }
+    }
+}
---
+++ WindowsServiceTest2/Service1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+
+
+namespace WindowsServiceTest2
+{
+    public partial class Service1 : ServiceBase
+    {
+        Logger logger;
+        public Service1()
+        {
+            InitializeComponent();
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+        }
+        protected override void OnStart(string[] args)
+        {
+            logger = new Logger();
+            Thread loggerThread = new Thread(new ThreadStart(logger.Start));
+            loggerThread.Start();
+        }
+        protected override void OnStop()
+        {
+            logger.Stop();
+            Thread.Sleep(1000);
+        }
+        protected override void OnContinue()
+        {
+
+        }
+        protected override void OnPause()
+        {
+
+        }
+
+    }
+
+    class Logger
+    {
+        FileSystemWatcher watcher;
+        object obj = new object();
+        bool enabled = true;
+        public Logger()
+        {
+            watcher = new FileSystemWatcher("C:\\TestTempo");
+            watcher.Deleted += Watcher_Deleted;
+            watcher.Created += Watcher_Created;
+            watcher.Changed += Watcher_Changed;
+            watcher.Renamed += Watcher_Renamed;
+        }
+
+        public void Start()
+        {
+            watcher.EnableRaisingEvents = true;
+            while (enabled)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+            enabled = false;
+        }
+
+        //Переименование файлов
+        private void Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            string fileEvent = "переименован в " + e.FullPath;
+            string filePath = e.OldFullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        //Изменение файлов
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "изменен";
+            string filePath = e.FullPath; ;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        //Создание файлов
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "Создан";
+            string filePath = e.FullPath; ;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        //Удаление файлов
+        private void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "удалён";
+            string filePath = e.FullPath; ;
+            RecordEntry(fileEvent, filePath);
+        }
+
+        private void RecordEntry(string fileEvent, string filePath)
+        {
+            lock (obj)
+            {
+                using (StreamWriter writer = new StreamWriter("C:\\templog.txt", true))
+                {
+                    writer.WriteLine(String.Format("{0} файл {1} был {2}",
+                        DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
+                    writer.Flush();
+                }
+            }
+        }
+    }
+}
